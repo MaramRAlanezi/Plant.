75271fec5095d3255ca4a8ae1fbe3a4a020d3493
MaramRAlanezi <malanezi24@twq.idserve.net>
2024-10-27 10:01:45 +0300

    Initial Commit

---
+++ Plant./Plant_App.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_App.swift
+//  Plant.
+//
+//  Created by Maram Rabeh  on 24/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Plant_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Plant.Tests/Plant_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Plant_Tests.swift
+//  Plant.Tests
+//
+//  Created by Maram Rabeh  on 24/04/1446 AH.
+//
+
+import Testing
+@testable import Plant_
+
+struct Plant_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
